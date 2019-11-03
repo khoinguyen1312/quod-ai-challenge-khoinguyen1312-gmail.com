74e9b86977a0476415bf7dbea814531fc496580a
Nguyen Phan <nguyen.phan1@mservice.com.vn>
2019-11-03 15:41:25 +0700

    Unit test for read file to temp file

--- src/test/java/ai/quod/challenge/GZipReaderTest.java
+++ src/test/java/ai/quod/challenge/GZipReaderTest.java
@@ -1,5 +1,13 @@
 package ai.quod.challenge;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.Optional;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -12,7 +20,15 @@ public class GZipReaderTest {
     }
 
     @Test
-    public void unzipFileToTempFile_should_unzipAFileToATempFolderSuccess() {
-        assert true;
+    public void unzipFileToTempFile_should_unzipAFileToATempFolderSuccess() throws IOException {
+        ClassLoader classLoader = this.getClass().getClassLoader();
+        File file = new File(classLoader.getResource("testFile.gz").getFile());
+
+        GZipReader gZipReader = new GZipReader();
+        Optional<Path> path = gZipReader.unzipFileToTempFile(file.getAbsolutePath());
+
+        List<String> strings = Files.readAllLines(path.get());
+
+        Assert.assertEquals("ThisLineIsForTesting", strings.get(0));
     }
 }
